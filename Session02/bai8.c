2024b7ce3cfc8223bff99b02f3149af45dadbcea
thạch lại lập trình <nguyenthachanhlctu@gmail.com>
2025-10-25 19:13:07 +0700

    Update bai8.c

--- Session02/bai8.c
+++ Session02/bai8.c
@@ -7,7 +7,7 @@ int main() {
     while (number > 0) {
         int sugoi = number % 10;        // lay so cuoi
         result = result * 10 + sugoi;   // dao nguoc chu so 12345 -> 54321
-        number = number / 10;           // bo chu so cuoi1/	1
+        number = number / 10;           // bo chu so cuoi
     }
 
     printf("So dao nguoc: %d\n", result);
